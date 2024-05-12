aa2f01ab3eb917e5b2509df3b21a4192ef7d7646
trueshizus <jesus.herrera.90@gmail.com>
2024-05-12 19:51:55 +0200

    chore: Add Google Fonts preconnect and update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Courier Prime', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
